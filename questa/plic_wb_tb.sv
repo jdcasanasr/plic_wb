ddf1f0bc384824ed3d4940e8b6b3cf405dc87c3b
Daniel Casañas <jdaniel.casanasr@gmailcom>
2025-03-06 14:20:52 -0600

    ToDo: Finish "plic_core" instantiation

---
+++ questa/plic_wb_tb.sv
@@ -0,0 +1,42 @@
+module plic_wb_tb.sv ();
+    // PLIC configuration.
+    localparam sources          = 16;
+    localparam targets          = 1;
+    localparam priorities       = 8;
+    localparam pending_requests = 4;
+
+    // PLIC inputs.
+    logic rst_nr;
+    logic clk_r;
+
+
+    // PLIC outputs.
+
+
+    // Module Instances.
+    plic_core plic_core_instance
+    #(
+        .SOURCES            (sources),
+        .TARGETS            (targets),
+        .PRIORITIES         (priorites),
+        .MAX_PENDING_COUNT  (pending_requests)
+    )
+    (
+        .rst_n              (rst_nr),
+        .clk                (clk_r),
+
+        .src                (),
+        .el                 (),
+        .ip                 (),
+
+        .ie                 (),
+        .ipriority          (),
+        .threshold          (),
+
+        .ireq               (),
+        .id                 (),
+        .claim              (),
+        .complete           ()
+    );
+
+endmodule
